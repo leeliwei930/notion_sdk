802465182b610791766789177f9fb9b186fd5799
Lee Li Wei <leeliwei930@gmail.com>
2023-03-19 00:15:57 +0800

    add contains and not contains props to filter.Select

--- database/filter/select.go
+++ database/filter/select.go
@@ -4,8 +4,10 @@ package filter
 type Status = Select
 
 type Select struct {
-	Equals     string `json:"equals,omitempty"`
-	NotEqual   string `json:"does_not_equal,omitempty"`
-	IsEmpty    bool   `json:"is_empty,omitempty"`
-	IsNotEmpty bool   `json:"is_not_empty,omitempty"`
+	Equals      string `json:"equals,omitempty"`
+	NotEqual    string `json:"does_not_equal,omitempty"`
+	IsEmpty     bool   `json:"is_empty,omitempty"`
+	IsNotEmpty  bool   `json:"is_not_empty,omitempty"`
+	Contains    string `json:"contains,omitempty"`
+	NotContains string `json:"does_not_contains,omitempty"`
 }
